e145fec68b2cb40da6a97f310b7698a910262090
Arjun Soni <94898141+ArjunKSoni@users.noreply.github.com>
2023-05-30 11:54:18 +0530

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BiDotsVerticalRounded, BiSearchAlt2 } from 'react-icons/bi';
 import { BsFillChatDotsFill } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function Home() {
+    const navigate = useNavigate()
+    const [logout, setout] = useState("block")
     return (
         <div className='h-screen'>
             <div style={{ backgroundColor: "rgb(40, 39, 39)", height: "15%" }} className='p-3'>
                 <div className='flex items-center mb-2 justify-between'>
                     <h2 className='text-gray-300 font-bold text-xl'>GeeksUp</h2>
-                    <BiDotsVerticalRounded className='text-2xl text-gray-300' />
+                    <BiDotsVerticalRounded onClick={(e) => { logout === "none" ? setout("block") : setout("none") }} className='text-2xl text-gray-300' />
+                    <div style={{ display: logout, width: "100px" }} className='absolute rounded-xl overflow-hidden bg-black top-12 right-0 z-10'>
+                        <button className='w-full p-3  bg-black  hover:bg-gray-700 font-bold text-white'>Profile</button>
+                        <button onClick={(e) => { e.preventDefault(); navigate("/") }} className='w-full hover:bg-gray-700 p-3 bg-black font-bold text-white'>Logout</button>
+                    </div>
                 </div>
                 <div className='w-full rounded-full py-1 input px-4 flex items-center justify-between bg-slate-600 '>
                     <input placeholder='Search Id..' className='' type="text" />
                     <BiSearchAlt2 className='text-2xl text-gray-300' />
                 </div>
             </div>
-            <div style={{ backgroundColor: "rgb(18, 18, 18)", height: "85%" }} className=''>
+            <div style={{ backgroundColor: "rgb(18, 18, 18)", height: "85%" }} className=' overflow-scroll'>
                 hi
             </div>
             <div style={{ width: "60px", height: "60px" }} className='absolute flex  items-center justify-center bg-green-500 text-white rounded-full bottom-6 right-6'>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 export default function Login() {
+    const navigate = useNavigate()
     return (
         <div className='h-screen w-screen flex flex-col login-main items-center justify-center'>
             <h2 className='text-gray-200 font-bold text-3xl -mt-16 mb-16'>GeeksUp</h2>
             <h2 style={{ borderBottom: "solid green 3px" }} className='text-black font-bold -mt-16 mb-16'>LogIn</h2>
             <div style={{ height: "50%", width: "100%" }} className='login input2 flex items-center justify-center flex-col'>
-                <input className='mb-4' type="email" placeholder='email' name="" id="" />
-                <input className='mb-4' type="password" placeholder='password' />
-                <button className='text-white fotn-bold text-lg bg-green-500 py-2 px-3 rounded-lg'>Log In</button>
+                <input className='mb-4' type="email" placeholder='email' name="" id="email" />
+                <input className='mb-4' type="password" placeholder='password' id='password' />
+                <button onClick={(e) => { e.preventDefault(); navigate("/home") }} className='text-white fotn-bold text-lg bg-green-500 py-2 px-3 rounded-lg'>Log In</button>
                 <h2 className='text-white'>Didn't have an Account? <Link to={"/signup"} className='text-green-500'>Register</Link></h2>
             </div>
             <div className='absolute flex items-center justify-center px-1 bottom-5 text-lg left-0 w-screen'>
