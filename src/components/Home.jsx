6e214d394b82b7e796302ed0f1e1a2b45070ecda
Arjun Soni <94898141+ArjunKSoni@users.noreply.github.com>
2023-05-30 09:58:33 +0530

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { BiDotsVerticalRounded, BiSearchAlt2 } from 'react-icons/bi';
 
+
 export default function Home() {
     return (
         <div className='h-screen'>
-            <div style={{ backgroundColor: "rgb(40, 39, 39)" }} className='p-3'>
+            <div style={{ backgroundColor: "rgb(40, 39, 39)", height: "15%" }} className='p-3'>
                 <div className='flex items-center mb-2 justify-between'>
                     <h2 className='text-gray-300 font-bold text-xl'>GeeksUp</h2>
                     <BiDotsVerticalRounded className='text-2xl text-gray-300' />
@@ -14,7 +15,7 @@ export default function Home() {
                     <BiSearchAlt2 className='text-2xl text-gray-300' />
                 </div>
             </div>
-            <div style={{ backgroundColor: "rgb(18, 18, 18)" }} className='h-full'>
+            <div style={{ backgroundColor: "rgb(18, 18, 18)", height: "85%" }} className=''>
                 hi
             </div>
         </div>
