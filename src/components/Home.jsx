f2a76520b25d87baa2dcb65ef494a84334997c11
Arjun Soni <94898141+ArjunKSoni@users.noreply.github.com>
2023-05-30 12:09:49 +0530

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,9 @@ import { useNavigate } from 'react-router-dom';
 
 
 export default function Home() {
+    const [chat, setchat] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]);
     const navigate = useNavigate()
-    const [logout, setout] = useState("block")
+    const [logout, setout] = useState("none")
     return (
         <div className='h-screen'>
             <div style={{ backgroundColor: "rgb(40, 39, 39)", height: "15%" }} className='p-3'>
@@ -23,8 +24,22 @@ export default function Home() {
                     <BiSearchAlt2 className='text-2xl text-gray-300' />
                 </div>
             </div>
-            <div style={{ backgroundColor: "rgb(18, 18, 18)", height: "85%" }} className=' overflow-scroll'>
-                hi
+            <div style={{ backgroundColor: "rgb(18, 18, 18)", height: "85%" }} className='text-white overflow-scroll'>
+                {chat.map((e, i) => {
+                    return (
+                        <div key={i} className='m-4 pb-2 flex items-center justify-between'>
+                            <div className='flex items-center justify-center'>
+                                <div style={{ width: "50px", height: "50px" }} className='rounded-full bg-white overflow-hidden'><img src="boy.png" width={"60px"} alt="" /></div>
+                                <div className='ml-3'>
+                                    <h1 className='font-bold text-xl'>Chat Name</h1>
+                                    <p className='ml-3 text-slate-400'>inner chats</p>
+                                </div>
+                            </div>
+
+                            <div>Time</div>
+                        </div>
+                    )
+                })}
             </div>
             <div style={{ width: "60px", height: "60px" }} className='absolute flex  items-center justify-center bg-green-500 text-white rounded-full bottom-6 right-6'>
                 <BsFillChatDotsFill className='text-3xl' />
