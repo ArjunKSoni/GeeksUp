480606d4a50538ef24cc8464e53d144a0d51f1fe
Arjun Soni <94898141+ArjunKSoni@users.noreply.github.com>
2023-05-30 09:47:51 +0530

    geeksup

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Chat from './components/Chat';
+import Request from './components/Request';
+import Search from './components/Search';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path={`/login`} element={<Login />} />
+          <Route exact path='/signup' element={<Signup />} />
+          <Route exact path='/chat' element={<Chat />} />
+          <Route exact path='/request' element={<Request />} />
+          <Route exact path='/search' element={<Search />} />
+          <Route exact path='/' element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BiDotsVerticalRounded, BiSearchAlt2 } from 'react-icons/bi';
+
+export default function Home() {
+    return (
+        <div className='h-screen'>
+            <div style={{ backgroundColor: "rgb(40, 39, 39)" }} className='p-3'>
+                <div className='flex items-center mb-2 justify-between'>
+                    <h2 className='text-gray-300 font-bold text-xl'>GeeksUp</h2>
+                    <BiDotsVerticalRounded className='text-2xl text-gray-300' />
+                </div>
+                <div className='w-full rounded-full py-1 input px-4 flex items-center justify-between bg-slate-600 '>
+                    <input placeholder='Search Id..' className='' type="text" />
+                    <BiSearchAlt2 className='text-2xl text-gray-300' />
+                </div>
+            </div>
+            <div style={{ backgroundColor: "rgb(18, 18, 18)" }} className='h-full'>
+                hi
+            </div>
+        </div>
+    )
+}
